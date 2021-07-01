ca936f6390a0a9cfa8ebdf593d388a22c5364ab9
tyler <tnegro@gmail.com>
2021-06-30 19:28:56 -0600

    add jest test config and first test

--- pages/api/users.js
+++ pages/api/users.js
@@ -3,5 +3,5 @@ const users = [{ id: 1 }, { id: 2 }, { id: 3 }]
 
 export default function handler(req, res) {
   // Get data from your database
-  res.status(404).json(users)
+  res.status(200).json(users)
 }
--- utils/extract-names.test.js
+++ utils/extract-names.test.js
@@ -3,9 +3,9 @@ import { transformNameData } from './extract-names.js'
 describe('extracting names util', () => {
   it('should extract names from array and transfrom them', () => {
     const mockData = ['Test', 'F', '1000', 'TestName2', 'M', '2000']
-    expect(transformNameData(transformNameData)).toBe([
-      { name: 'Test', sex: 'F', popularity: '1000' },
-      { name: 'TestName2', sex: 'M', popularity: '2000' },
+    expect(transformNameData(mockData)).toMatchObject([
+      { name: 'Test', sex: 'F', popularity: '1000', year: '2020' },
+      { name: 'TestName2', sex: 'M', popularity: '2000', year: '2020' },
     ])
   })
 })
