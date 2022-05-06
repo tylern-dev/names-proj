d33a1c48dc026fa7a91c5e04f1d4d8f091b6c692
Tyler Negro <tnegro@gmail.com>
2022-05-06 05:39:27 +0000

    feat(auth): handle logouts and redirects

--- client/src/ClientRoutes.tsx
+++ client/src/ClientRoutes.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import {
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+  useLocation,
+} from 'react-router-dom'
 import AuthWall from './components/AuthWall/AuthWall'
+import { useAuthContext } from './hooks/AuthProvider'
 import Login from './pages/Login/Login'
 import Name from './pages/Name'
 const ClientRoutes = () => {
+  const { loading } = useAuthContext()
+  const location = useLocation()
+
+  if (loading) return <>Loading...</>
+
+  // this may be a temporary redirect
+  if (location.pathname === '/') return <Navigate to="/login" replace />
+
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route
-        path="dashboard"
+        path="/dashboard"
         element={
           <AuthWall>
             <Name />
